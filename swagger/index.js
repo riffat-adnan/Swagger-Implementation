9e9316adce91e8c9b7da0acfc8cb4923be16a104
riffatadnan <riffatadnan175@gmail.com>
2024-09-13 19:49:59 +0500

    Removed unused code and udpated the readme

--- swagger/index.js
+++ swagger/index.js
@@ -1,23 +1,8 @@
 const info = require("./info");
 const components = require("./components");
 const servers = require("./servers");
-const test = require("./test");
-const state = require("./state");
-const governingBody = require("./governingBody");
-const governingSubBody = require("./governingSubBody");
 const tags = require("./tags");
 const auth = require("./auth");
-const tournament = require("./tournament");
-const team = require("./team");
-const mat = require("./mat");
-const group = require("./group");
-const division = require("./division");
-const statistician = require("./statistician");
-const weightClass = require("./weightClass");
-const tournamentBrackets = require("./tournamentBrackets");
-const tournamentTieBreakers = require("./tournamentTieBreaker");
-const athlete = require("./athlete");
-const SkinFoldTest = require("./SkinFoldTest");
 
 module.exports = {
   ...info,
@@ -26,22 +11,7 @@ module.exports = {
   ...tags,
   ...{
     paths: {
-      ...test,
-      ...state,
-      ...governingBody,
-      ...governingSubBody,
       ...auth,
-      ...tournament,
-      ...team,
-      ...statistician,
-      ...group,
-      ...division,
-      ...mat,
-      ...weightClass,
-      ...tournamentBrackets,
-      ...tournamentTieBreakers,
-      ...athlete,
-      ...SkinFoldTest
     },
   },
 };
--- swagger/tags.js
+++ swagger/tags.js
@@ -1,46 +1,7 @@
 module.exports = {
   tags: [
     {
-      name: "Test",
-    },
-    {
-      name: "State",
-    },
-    {
-      name : "Governing Body"
-    },
-    {
-      name : "Governing Sub Body"
-    },
-    {
       name: "Auth",
     },
-    {
-      name: "Tournament",
-    },
-    {
-      name: "Team",
-    },
-    {
-      name: "Statistician",
-    },
-    {
-      name: "Group",
-    },
-    {
-      name: "Division",
-    },
-    {
-      name: "Mat",
-    },
-    {
-      name : "Tournament Bracket"
-    },
-    {
-      name : "Athlete"
-    },
-    {
-      name : "Skin Fold Test"
-    }
   ],
 };
